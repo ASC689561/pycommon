be170d8b15b85318e355cd9b8f9c4b39995f0516
asc686f61 <asc686f61@gmail.com>
2017-11-09 08:48:02 +0700

    Thêm logger builder

--- pycommon/json_util.py
+++ pycommon/json_util.py
@@ -6,6 +6,10 @@ def json2obj(json_str):
     try:
         obj = json.loads(json_str)
         return obj
+
+    except json.JSONDecodeError as ex:
+        logging.error("Parse json error: " + json_str)
+        raise ex
     except ValueError as ve:
         logging.error("Parse json error: " + json_str)
         raise ve
--- pycommon/logging_util.py
+++ pycommon/logging_util.py
@@ -2,23 +2,26 @@ import logging
 import os
 from logging.handlers import RotatingFileHandler
 
+from logstash import TCPLogstashHandler
+
 
 class LogBuilder:
     def __init__(self):
         self.handlers = []
-        self.format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-        pass
+        self.format_str = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
     def build(self):
-        logging.basicConfig(level=logging.DEBUG, format=self.format, handlers=self.handlers)
+        logging.basicConfig(level=logging.DEBUG, format=self.format_str, handlers=self.handlers)
 
-    def init_stream_handler(self, *levels):
-        for v in levels:
-            handler = logging.StreamHandler()
-            handler.setLevel(v)
-            self.handlers.append(handler)
+    def set_format(self, format_str):
+        self.format_str = format_str
+        return self
 
-    def init_file_log(self, log_path):
+    def init_stream_handler(self):
+        self.handlers.append(logging.StreamHandler())
+        return self
+
+    def init_rotating_file(self, log_path):
         if not os.path.exists(log_path):
             os.makedirs(log_path)
 
@@ -32,3 +35,12 @@ class LogBuilder:
         error_log.setLevel(logging.ERROR)
 
         self.handlers.extend([debug_log, info_log, error_log])
+        return self
+
+    def init_logstash(self, host, port, *levels):
+        for v in levels:
+            logstash_handler = TCPLogstashHandler(host, port, version=1)
+            logstash_handler.setLevel(v)
+            self.handlers.append(logstash_handler)
+
+        return self
