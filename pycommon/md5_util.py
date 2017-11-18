d33d4cbe40f4523fd7898c9963437970f9fb43ca
asc686f61 <asc686f61@gmail.com>
2017-11-18 09:21:36 +0700

    sua ten md5 dic

--- pycommon/md5_util.py
+++ pycommon/md5_util.py
@@ -4,3 +4,11 @@ def get_dic_md5(dic):
 
     data_md5 = hashlib.md5(json.dumps(dic, sort_keys=True).encode()).hexdigest()
     return str(data_md5)
+
+
+def get_md5(dic):
+    import hashlib
+    import json
+
+    data_md5 = hashlib.md5(json.dumps(dic, sort_keys=True).encode()).hexdigest()
+    return str(data_md5)
