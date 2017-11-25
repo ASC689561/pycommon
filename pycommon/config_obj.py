8e1e5ee254def0f4a465d21ebb3c98fe6f52c322
asc686f61 <asc686f61@gmail.com>
2017-11-25 09:53:15 +0700

    them obj_config

--- pycommon/config_obj.py
+++ pycommon/config_obj.py
@@ -1,34 +1,51 @@
-# import configparser
-#
-#
-# class ObjectConfig:
+import configparser
+
+
+class ObjectConfig:
+    def __init__(self):
+        pass
+
+    def merge_file(self, file_path, session_name='DEFAULT'):
+        config = configparser.ConfigParser()
+        config.read(file_path)
+        session_value = config[session_name]
+        self_attr = list(self._get_all_attr())
+
+        for v in list(session_value):
+            for k in self_attr:
+                if v == k.lower():
+                    setattr(self, k, session_value[v])
+
+    # def merge_env(self, *list_property_name):
+    #     import os
+    #     self_attr = list(self._get_all_attr())
+    #     for v in self_attr:
+    #         if v in os.environ:
+    #             setattr(self, v, os.environ[v])
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def _get_all_attr(self):
+        attrs = dir(self)
+        for v in attrs:
+            if str.startswith(v, '__'):
+                continue
+            yield v
+
+
+# class AmisConfig(ObjectConfig):
 #     NluProject = None
+#     SmeService = None
+#     BotDataService = None
+#     USERNAME = None
 #
-#     def __init__(self, config_file):
-#         config = configparser.ConfigParser()
-#         config.read(config_file)
-#
-#         self.__set_value()
-#         pass
-#
-#     def __get_env_or_config(key, default_value=None, session='DEFAULT'):
-#         session = config[session]
-#         if key in os.environ:
-#             return os.environ[key]
-#
-#         value = session.get(key, None)
-#         if value is None:
-#             return default_value
-#         return value
-#
-#     def __set_value(self):
-#         attrs = dir(self)
-#         for v in attrs:
-#             if str.startswith(v, '__'):
-#                 continue
-#             print(v)
-#             value = get_env_or_config(v)
-#             setattr(self, v, value)
-#
-#     def list_all(self):
-#         pass
+#     def __init__(self):
+#         super().__init__()
+
+
+# obj = AmisConfig()
+# obj.merge_file('/opt/projects/bot/botmit/botmit/config.ini')
+# obj.merge_env('USERNAME','SmeService')
+# print(obj.NluProject)
+# print(obj)
