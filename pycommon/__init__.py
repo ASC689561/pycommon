7484868043df16faf826ce96ee79716dccaadbf5
asc689561 <asc689561@gmail.com>
2017-11-26 23:44:13 +0700

    Sửa init.py

--- pycommon/__init__.py
+++ pycommon/__init__.py
@@ -1,11 +1,15 @@
+from .cache import cache, init_diskcache
 from .cfg_reader import *
+from .config_base import ConfigBase
 from .curl_util import *
 from .function_util import *
+from .json_util import *
+from .logging_util import *
 from .md5_util import *
 from .path_util import *
 from .string_util import *
-from .json_util import *
 from .try_catch_util import *
-from .logging_util import *
-from .cache import cache, init_diskcache
-from .config_base import ConfigBase
+
+path = os.path.dirname(os.path.abspath(__file__)) + "/patterns"
+print(path)
+sys.path.append(path)
