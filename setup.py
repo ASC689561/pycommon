36ef664e226cf6e5b6dd8afd00dadd877d5d0645
asc686f61 <asc686f61@gmail.com>
2017-11-27 09:44:50 +0700

    thêm timming function

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pycommon',
     version='1.0.0',
     # packages=find_packages(exclude=('tests', 'common.egg-info'),include=['*.py','processor/*.py']),
-    packages=['pycommon'],
+    packages=['pycommon','pycommon.patterns'],
     zip_safe=False,
     install_requires=['pymongo', 'logstash', 'python-logstash','diskcache']
 )
