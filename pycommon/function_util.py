83e79d33a03dcbb760d552080c669416f10ce411
asc686f61 <asc686f61@gmail.com>
2017-11-29 10:59:31 +0700

    Them custom timing log

--- pycommon/function_util.py
+++ pycommon/function_util.py
@@ -4,16 +4,17 @@ import logging
 import time
 
 
-def custom_timing_log(func):
+def custom_timing_log(func, name=None):
     def timing(f):
         def wrap(*args):
             time1 = time.time()
             ret = f(*args)
             time2 = time.time()
             log_time = (time2 - time1) * 1000.0
-            # logging.log(60, '%s function took %0.3f ms' % (f.__name__, log_time), extra={f.__name__: str(log_time)})
-
-            func(f.__name__, log_time)
+            if name is None:
+                func(f.__name__, log_time)
+            else:
+                func(name, log_time)
             return ret
 
         return wrap
