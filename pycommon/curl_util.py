a1001e866e1679b86800074d7df82ded746eb67c
nbhoa <asc686f61@gmail.com>
2017-12-11 09:53:48 +0700

    sua curl

--- pycommon/curl_util.py
+++ pycommon/curl_util.py
@@ -1,14 +1,17 @@
 import logging
 
 
-def execute_curl(curl):
+def execute_curl(curl, json_out=True):
     import json
     import subprocess
+    out = ''
     try:
         p = subprocess.Popen(curl, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
-        obj = json.loads(out.decode())
-        return obj
+        if json_out:
+            return json.loads(out.decode())
+        else:
+            return out
     except Exception as e:
-        logging.error('Error when execute url')
+        logging.error('Error when execute curl, resull[{}]'.format(out))
         raise e
